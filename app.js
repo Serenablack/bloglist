873d130a4c72c854238ad1f5aaedc0766b3812f9
Serena Black <sunsetsin1.sy@gmail.com>
2022-08-15 16:37:34 +0545

    ex-4.18 token authentication done

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const config = require("./utils/config");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
 
+const loginRouter = require("./controllers/login");
+
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 // const mongoUrl = "mongodb://localhost/bloglist";
@@ -25,6 +27,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 // const PORT = 3003;
 // app.listen(PORT, () => {
