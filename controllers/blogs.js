b2ed3f83f20e63f5c7a0426b50d35a97c3c86ff8
Serena Black <sunsetsin1.sy@gmail.com>
2022-08-15 16:01:15 +0545

    completed ex 4.19

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,10 +1,12 @@
 const blogsRouter = require("express").Router();
 const { response } = require("express");
 const Blog = require("../models/blog");
+const User = require("../models/user");
+const helper = require("../tests/test_helper");
 
 blogsRouter.get("/", async (request, response, next) => {
   try {
-    const blogs = await Blog.find({});
+    const blogs = await Blog.find({}).populate("user");
     response.json(blogs);
   } catch (error) {
     next(error);
@@ -13,6 +15,13 @@ blogsRouter.get("/", async (request, response, next) => {
 
 blogsRouter.post("/", async (request, response, next) => {
   const blog = new Blog(request.body);
+  const UsersInDb = await helper.usersInDb();
+  let randomUser = UsersInDb[Math.floor(Math.random() * UsersInDb.length)];
+  const username = randomUser.username;
+
+  const user = await User.findOne({ username });
+  console.log(user);
+
   if (!blog.title && !blog.url) {
     response.status(400).json("Bad Request");
   } else {
@@ -20,7 +29,10 @@ blogsRouter.post("/", async (request, response, next) => {
       blog["likes"] = 0;
     }
     try {
+      blog["user"] = user._id;
       const result = await blog.save();
+      user.blogs = user.blogs.concat(result._id);
+      await user.save();
       response.status(201).json(result);
     } catch (error) {
       next(error);
